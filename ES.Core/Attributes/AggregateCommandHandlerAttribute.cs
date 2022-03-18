b7a80546225e7057d1070ee8f0aae196bd670b75
OV <ointernet@pm.me>
2022-03-18 15:08:37 -0500

    Make attribute properties get only

--- ES.Core/Attributes/AggregateCommandHandlerAttribute.cs
+++ ES.Core/Attributes/AggregateCommandHandlerAttribute.cs
@@ -3,7 +3,7 @@ namespace ES.Core.Attributes;
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public sealed class AggregateCommandHandlerAttribute : Attribute
 {
-    public string CommandName { get; private set; }
+    public string CommandName { get; }
 
     public AggregateCommandHandlerAttribute(string commandName)
     {
