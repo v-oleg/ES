793eb68834366733fa89b723677b9e2f5b282427
OV <ointernet@pm.me>
2022-03-19 18:56:12 -0500

    Add ignore attribute.
    Used to ignore projectors for now, can be extended

---
+++ ES.Core/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,8 @@
+namespace ES.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+// ReSharper disable once ClassNeverInstantiated.Global
+public class IgnoreAttribute : Attribute
+{
+    
+}
